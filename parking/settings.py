a93e4daa9f612b3f7757afae8483fd5583da4f55
roggerfq <roger.figueroa@javerianacali.edu.co>
2021-02-02 17:01:29 -0500

    Added files and changes required for deployment to heroku2

--- parking/settings.py
+++ parking/settings.py
@@ -120,16 +120,27 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
